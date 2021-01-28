bfae503d00796b7edab23fe897dcb6dedec48ae9
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-01-28 22:14:25 +0300

    uploading

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
-
-// function getPromise(ispassed) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             if(ispassed) {
-//                 resolve('i have passed the exam')
-//             }else {
-//                 reject(new Error('i am failed'))
-//             }
-//         },2000)
-//     })
-// }
-
-// getPromise(false)
-// .then((res) => {
-//     console.log(res)
-// })
-// .catch((e) => {
-//     console.log(e)
-// })
-
-// const delay = s => new Promise(resolve => setTimeout(resolve, 1000 * s))
-
-// delay(2).then(() => console.log('after 2 second'))
-// delay(3).then(() => console.log('after 3 second'))
-// delay(5).then(() => console.log('after 5 second'))
-// delay(2).then(() => console.log('after 2 second'))
+JUST FOR PRACTICE
 
 
+function getPromise(ispassed) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(ispassed) {
+                resolve('i have passed the exam')
+            }else {
+                reject(new Error('i am failed'))
+            }
+        },2000)
+    })
+}
 
-// let p1 = new Promise(resolve => {
-//     setTimeout(resolve, 5000, 'one')
-// })
+getPromise(false)
+.then((res) => {
+    console.log(res)
+})
+.catch((e) => {
+    console.log(e)
+})
 
-// let p2 = new Promise(resolve => {
-//     setTimeout(resolve, 300, 'two')
-// })
+const delay = s => new Promise(resolve => setTimeout(resolve, 1000 * s))
 
-// Promise.all([p1,p2])
-// .then(res => console.log(res))
+delay(2).then(() => console.log('after 2 second'))
+delay(3).then(() => console.log('after 3 second'))
+delay(5).then(() => console.log('after 5 second'))
+delay(2).then(() => console.log('after 2 second'))
 
-// Promise.race([p1,p2])
-// .then(v => console.log(v))
+
+
+let p1 = new Promise(resolve => {
+    setTimeout(resolve, 5000, 'one')
+})
+
+let p2 = new Promise(resolve => {
+    setTimeout(resolve, 300, 'two')
+})
+
+Promise.all([p1,p2])
+.then(res => console.log(res))
+
+Promise.race([p1,p2])
+.then(v => console.log(v))
 
 
 let promises = [Promise.resolve(2),Promise.resolve(3),Promise.resolve(4)]
@@ -51,3 +53,72 @@ async function asyncAll() {
 }
 
 asyncAll()
+
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    let isvalid = false
+    let data = {};
+    [...this.elements].forEach(el => {
+        if(el.type !== 'submit') {
+            isvalid = validator(el)
+            if(isvalid) {
+                data[el.name] = el.value
+            }else {
+                alert(`require field need to fill up!`)
+                
+            }
+        }
+        console.log(data)
+    })
+    if(isvalid) {
+        console.log(data)
+        this.reset()
+    }
+})
+
+function validator(el) {
+    if(!el.value) {
+        return false
+    }
+    return true
+}
+
+var a 
+
+function async() {
+    a = 20
+    setTimeout(()=> {
+        console.log(a)
+    },)
+}
+
+async()
+
+a = 30
+
+
+class Person {
+    constructor(name) {
+        console.log(name, ' is a construtor')
+    }
+    getId() {
+        return 10
+    }
+}
+
+
+class AnotherPerson extends Person {
+    constructor(name){
+        console.log(name, ' constructor from another class')
+        super(name)
+    }
+}
+
+let p = new Person('rafi')
+console.log(p.getId())
+
+
